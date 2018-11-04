04ab5a0e0c5c2d63f402bb47c2575e9f118ef321
Deden Nugraha <deden@Pejantangguh.local>
2018-11-04 16:42:06 +0700

    basic-authorization

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { Client } = require("pg");
+const express = require('express')
+const bodyParser = require('body-parser')
+const { Client } = require('pg')
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Basic Authorization
+app.use((req, res, next) => {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization !== process.env.API_TOKEN
+  ) {
+    res.status(400)
+    return res.json({ error: 'Unauthorized (0)' })
+  }
+  next()
+})
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true
-});
+})
 
-client.connect();
+client.connect()
 
-app.get("/transactions", async (req, res) => {
+app.get('/transactions', async (req, res) => {
   try {
-    const transactions = await client.query("SELECT * FROM transactions;");
-    res.header("Content-Type", "application/json");
-    res.status(200);
-    return res.json({ data: transactions.rows });
+    const transactions = await client.query('SELECT * FROM transactions;')
+    res.header('Content-Type', 'application/json')
+    res.status(200)
+    return res.json({ data: transactions.rows })
   } catch (e) {
-    console.error(e);
-    res.status(500);
-    return res.json({ error: "Internal server error" });
+    console.error(e)
+    res.status(500)
+    return res.json({ error: 'Internal server error' })
   }
-});
+})
 
-app.all("*", (req, res) => {
-  return res.json({ data: "Hello world!" });
-});
+app.all('*', (req, res) => {
+  return res.json({ data: 'Hello world!' })
+})
 
-process.env.PORT = process.env.PORT || 3000;
-const port = process.env.PORT;
+process.env.PORT = process.env.PORT || 3000
+const port = process.env.PORT
 module.exports = app.listen(port, () => {
-  console.log(`Backend server started`);
-});
+  console.log(`Backend server started`)
+})
